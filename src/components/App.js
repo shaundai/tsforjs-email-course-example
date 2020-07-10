423b6cee3fe7f71de71698c92e4c9dd48bc8b8e3
Shaundai Person <shaundaiperson@gmail.com>
2020-07-10 00:25:35 -0400

    start app page

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 import './App.css';
+import salesloftlogo from '../images/salesloftlogo.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-          Welcome, User! This is where the app dashboard will be.
-      </header>
+        <Header>
+          <img alt="SalesLoft Logo" src={salesloftlogo} style={{height: '2em', marginLeft: '1em'}}/>
+        </Header>
+        Welcome, User!
+        This is where the app dashboard will be.
     </div>
   );
 }
 
 export default App;
+
+const Header = styled.div`
+  color: white;
+  background-color: #0d163a;
+  padding: .7em 0;
+`
