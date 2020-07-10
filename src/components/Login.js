433d56544232006c306f8c923882b9e3038c1156
Shaundai Person <shaundaiperson@gmail.com>
2020-07-09 23:12:30 -0400

    style login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,53 @@
 import React from 'react';
 import './App.css';
+import styled from 'styled-components';
+import salesloftlogo from '../images/salesloftlogo.png';
 
 const Login = () => {
-const redirectUri = 'https://shaundai-salesloft-node.herokuapp.com/salesloft'
-const clientId = '7278d0df3e6175f5900d67986ab4a639f38d80339d6692bc6d29ecb4a5e8d3ef'
+//const redirectUri = 'https://shaundai-salesloft-node.herokuapp.com/salesloft'
+//const clientId = '7278d0df3e6175f5900d67986ab4a639f38d80339d6692bc6d29ecb4a5e8d3ef'
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <a href='https://accounts.salesloft.com/oauth/authorize?client_id=7278d0df3e6175f5900d67986ab4a639f38d80339d6692bc6d29ecb4a5e8d3ef&redirect_uri=https://shaundai-salesloft-node.herokuapp.com/salesloft&response_type=code'>
-          Click to authenticate yourself as an actual user.
-        </a>
+    <Main>
+      <header>
+          <div>
+          <span style={{fontSize: '1.3em', margin: '.8em auto'}}>Welcome to</span>
+          <Header>Account-Based Selling</Header>
+          <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', margin: '.3em', fontSize: '1.3em'}}>
+              <em>by </em> <img alt="salesloft logo" src={salesloftlogo} style={{height: '1.6em', color: 'white', margin: '0 .4em'}} /></div>
+          </div>
+          <div style={{margin: '2.5em auto'}}>
+            <Button href='https://accounts.salesloft.com/oauth/authorize?client_id=7278d0df3e6175f5900d67986ab4a639f38d80339d6692bc6d29ecb4a5e8d3ef&redirect_uri=https://shaundai-salesloft-node.herokuapp.com/salesloft&response_type=code'>
+            Log in
+            </Button>
+        </div>
       </header>
-    </div>
+    </Main>
   );
 }
 
 export default Login;
+
+const Main = styled.div`
+    background-color: #42aedc;
+    height: 100vh;
+    color: white;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-family: Barlow;
+`
+const Header = styled.div`
+    font-size: 2.5em;
+    font-weight: 600;
+`
+
+const Button = styled.a`
+    background-color: #d47300;
+    color: 'white';
+    border-radius: 26px;
+    font-weight: 600;
+    padding: 9px 21px 10px 21px;
+    text-decoration: none;
+`
