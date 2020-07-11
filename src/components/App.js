9ae19906b3a9f1bcc69ccff98c56825fdded9f1b
Shaundai Person <shaundaiperson@gmail.com>
2020-07-10 23:05:20 -0400

    add tier boxes for app page

--- src/components/App.js
+++ src/components/App.js
@@ -15,8 +15,24 @@ function App() {
         </Welcome>
         <Welcome>How will you hit your quota this quarter?</Welcome>
         </div>
-        <div>
-          Tier 1
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '70vh'}}>
+        <TierBlock>
+          <TierTitle>Tier 1</TierTitle>
+          <ListAccount>
+            <div>
+            Account
+            </div>
+            
+            , Account Owner, # Employees:, Last Contacted: 
+            <div>Salesforce LinkedIn Website</div>
+          </ListAccount>
+        </TierBlock>
+        <TierBlock>
+          <TierTitle>Tier 2</TierTitle>
+        </TierBlock>
+        <TierBlock>
+          <TierTitle>Tier 3</TierTitle>
+        </TierBlock>
         </div>
     </div>
   );
@@ -32,5 +48,24 @@ const Header = styled.div`
 
 const Welcome = styled.div`
   padding-left: 1.5em;
-  padding-top: .7em
+  padding-top: .7em;
+`
+
+const TierBlock = styled.div`
+  border: 1px solid #e5e5e5;
+  width: calc(75%/3);
+  margin: 0 2em;
+  height: 60vh;
+  border-right: 2px solid rgba(0,0,0,.1);
+`
+const TierTitle = styled.div`
+  border-bottom: 2px solid rgba(0,0,0,.1);
+  background: linear-gradient(rgba(0, 0, 0, 0.03), rgba(0, 0, 0, 0.03)),#fff;
+  text-align: center;
+  font-weight: bold;
+  padding: .5em 0;
+`
+const ListAccount = styled.div`
+  border-bottom: 1px solid rgba(0,0,0,.1);
+  padding: .5em;
 `
