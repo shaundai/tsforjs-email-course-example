94bac05134d430e717d06621e01a57cc1be7bbc1
Shaundai Person <shaundaiperson@gmail.com>
2020-08-19 16:52:09 -0400

    make updates to send to production

--- src/components/Login.js
+++ src/components/Login.js
@@ -14,7 +14,7 @@ const Login = () => {
               <em>by </em> <img alt="salesloft logo" src={salesloftlogo} style={{height: '1.6em', color: 'white', margin: '0 .4em'}} /></div>
           </div>
           <div style={{margin: '2.5em auto'}}>
-            <Button href='https://accounts.salesloft.com/oauth/authorize?client_id=7278d0df3e6175f5900d67986ab4a639f38d80339d6692bc6d29ecb4a5e8d3ef&redirect_uri=http://localhost:3000/redirect&response_type=code'>
+            <Button href='https://accounts.salesloft.com/oauth/authorize?client_id=7278d0df3e6175f5900d67986ab4a639f38d80339d6692bc6d29ecb4a5e8d3ef&redirect_uri=https://shaundai-salesloft.surge.sh/redirect&response_type=code'>
             Log in
             </Button>
         </div>
--- src/components/util/salesloftApi.js
+++ src/components/util/salesloftApi.js
@@ -1,58 +1,60 @@
 const axios = require('axios');
 
+const path = "https://shaundai-salesloft-node.herokuapp.com"
+
 const SalesLoft = {
         
         getCurrentUserInfo(){
-                return axios.get(`http://localhost:3001/api/user`)
+                return axios.get(`${path}/api/user`)
         },
 
         getAnyUserInfo(userId){
-                return axios.get(`http://localhost:3001/api/team/id/${userId}`)
+                return axios.get(`${path}/api/team/id/${userId}`)
         },
 
         getAllAccountInfo(userId){
-                return axios.get(`http://localhost:3001/api/accounts?sort_by=last_contacted_at&per_page=100&owner_id%5B%5D=${userId}`)
+                return axios.get(`${path}/api/accounts?sort_by=last_contacted_at&per_page=100&owner_id%5B%5D=${userId}`)
         },
                 //AccountIds below needs to be an array with commas
         getPeopleAtAccounts(AccountIds){
-                return axios.get(`http://localhost:3001/api/accounts/people?account_id%5B%5D=${AccountIds}`)
+                return axios.get(`${path}/api/accounts/people?account_id%5B%5D=${AccountIds}`)
         },
 
         getSingleAccountInfo(AccountIds){
-                return axios.get(`http://localhost:3001/api/account/${AccountIds}`)
+                return axios.get(`${path}/api/account/${AccountIds}`)
         },
 
         getIdsOfCadencesByPerson(PersonId){
-                return axios.get(`http://localhost:3001/api/cadence/current/${PersonId}`)
+                return axios.get(`${path}/api/cadence/current/${PersonId}`)
         },
 
         //CadenceIds below needs to be an array with commas
         getCadenceNameById(CadenceIds){
-                return axios.get(`http://localhost:3001/api/cadence/name?cadenceid%5B%5D=${CadenceIds}`)
+                return axios.get(`${path}/api/cadence/name?cadenceid%5B%5D=${CadenceIds}`)
         },
 
         removePersonFromCadence(CadenceMembershipId){
-                return axios.get(`http://localhost:3001/api/cadence/remove/${CadenceMembershipId}`)
+                return axios.get(`${path}/api/cadence/remove/${CadenceMembershipId}`)
         },
 
         addPersonToCadence(PersonId, CadenceId){
-                return axios.get(`http://localhost:3001/api/cadence/add?personid=${PersonId}&cadenceid=${CadenceId}`)
+                return axios.get(`${path}/api/cadence/add?personid=${PersonId}&cadenceid=${CadenceId}`)
         },
 
         getContactInfo(PersonId){
-                return axios.get(`http://localhost:3001/api/people/id/${PersonId}`)
+                return axios.get(`${path}/api/people/id/${PersonId}`)
         },
 
         getCadenceMembershipByPersonIdAndCadenceId(PersonId, CadenceId){
-                return axios.get(`http://localhost:3001/api/cadence/membership?personid=${PersonId}&cadenceid=${CadenceId}`)
+                return axios.get(`${path}/api/cadence/membership?personid=${PersonId}&cadenceid=${CadenceId}`)
         },
 
         getAllCadencesByPersonGuid(TeamCadence, PersonGuid){
-                return axios.get(`http://localhost:3001/api/cadences/all?teamcadence=${TeamCadence}&ownerguid%5B%5D=${PersonGuid}`)
+                return axios.get(`${path}/api/cadences/all?teamcadence=${TeamCadence}&ownerguid%5B%5D=${PersonGuid}`)
         },
         
         getStepsOnCadence(CadenceId){
-                return axios.get(`http://localhost:3001/api/cadence/steps?cadenceid=${CadenceId}`)
+                return axios.get(`${path}/api/cadence/steps?cadenceid=${CadenceId}`)
         }
 }
 
