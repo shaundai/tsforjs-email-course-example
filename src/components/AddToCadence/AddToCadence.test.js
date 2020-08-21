c7a6120a9917f24a17b31fa8cd18366f4361e537
Shaundai Person <shaundaiperson@gmail.com>
2020-08-20 21:51:28 -0400

    complete tests for addtocadence

--- src/components/AddToCadence/AddToCadence.test.js
+++ src/components/AddToCadence/AddToCadence.test.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
-import * as File from '../AddToCadence';
-import * as CadenceIds from '../getCadenceIds'
+import * as File from './AddToCadence';
+import * as CadenceIds from './getCadenceIds'
 
 describe('AddToCadence', () => {
     beforeEach(() => jest.spyOn(CadenceIds, 'getCadenceIdsByGuid').mockImplementation(() => Promise.resolve([1234343, 10293202, 29302393])))
     afterEach(() => jest.clearAllMocks())
-    test('<AddToCadence /> shows Modal with close button', () => {
+    test('<AddToCadence /> shows Modal with close button on click', async () => {
 
-        const { getByText } = render(<File.AddToCadence />)
+        const { getByText, findByText } = render(<File.AddToCadence />)
         expect(getByText('+ Add to Cadence')).toBeTruthy()
     
         fireEvent.click(getByText('+ Add to Cadence'))
+        expect(await findByText(/Add People To Cadence/i)).toBeInTheDocument();
+        expect(await findByText(/Cancel/i)).toBeInTheDocument();
       });
 })
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -11,11 +11,11 @@ describe('App', () => {
     afterEach(() => jest.clearAllMocks())
     const { getByText } = render(<File.App userInfo={userInfo} list={allAccountInfo} />);
     
-    expect(getByText(/welcome/i)).toBeInTheDocument();
+      expect(getByText(/welcome/i)).toBeInTheDocument();
 
-    expect(getByText(/Tier 1/i)).toBeTruthy()
-    expect(getByText(/Tier 2/i)).toBeTruthy()
-    expect(getByText(/Tier 3/i)).toBeTruthy()
+      expect(getByText(/Tier 1/i)).toBeTruthy()
+      expect(getByText(/Tier 2/i)).toBeTruthy()
+      expect(getByText(/Tier 3/i)).toBeTruthy()
     });
 
     test('Correct username appears after api call', async () => {
