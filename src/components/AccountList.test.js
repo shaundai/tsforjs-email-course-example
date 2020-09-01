57aab34c6ba740433c05286456741efb54b64e85
Shaundai Person <shaundaiperson@gmail.com>
2020-09-01 17:34:52 -0400

    small update to tests

--- src/components/AccountList.test.js
+++ src/components/AccountList.test.js
@@ -7,12 +7,12 @@ const tier = 1
 
 describe('AccountList', () => {
     afterEach(() => jest.clearAllMocks())
-    it('AccountList returns correct number of accounts', () => {
+    it('returns correct number of accounts', () => {
         const { getAllByText } = render(<File.AccountList tier={tier} accounts={allAccountInfo} showPeopleList={false} />)
         expect(getAllByText(/Last Contacted/i)).toHaveLength(3)
       });
 
-      it('AccountList renders icons with icons for website, SFDC, and LinkedIn for each account', async () => {
+      it('renders icons with icons for website, SFDC, and LinkedIn for each account', async () => {
         const { getAllByRole, getAllByTestId } = render(<File.AccountList tier={tier} accounts={allAccountInfo} showPeopleList={false} />)
         expect(getAllByRole('img')).toHaveLength(9)
         expect(getAllByTestId('salesforce')).toHaveLength(3)
