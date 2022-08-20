19d1de8b4ea4dd2f03510b1d9e8a05436f28f2bb
Shaundai Person <shaundaiperson@gmail.com>
2022-08-20 14:49:42 -0400

    simplify app by removing files

--- src/components/AddToCadence/getCadenceIds.js
+++
@@ -1,9 +0,0 @@
-import SalesLoft from '../util/mockSalesloftApi'
-
-export const getCadenceIdsByGuid = async (isTeamCadence, userInfo) => {
-        const allCadencesById = (await SalesLoft.getAllCadencesByPersonGuid(isTeamCadence, userInfo.guid)).data.data
-        const justCadenceIds = allCadencesById.map((cadence) => {
-                return cadence.id
-            })
-        return justCadenceIds
-    }
--- src/components/util/Data.js/UserData.js
+++
@@ -1,5 +0,0 @@
-export const userInfo = {
-  id: 8686,
-  firstName: 'Oprah',
-  guid: 110617
-}
